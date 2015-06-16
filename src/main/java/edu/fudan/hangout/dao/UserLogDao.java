58044dd0e1b48179a289458a762857881d3bf72e
geeeeeeeeek <810421910@qq.com>
2015-06-16 10:55:29 +0800

    Meow

--- src/main/java/edu/fudan/hangout/dao/UserLogDao.java
+++
@@ -1,7 +0,0 @@
-package edu.fudan.hangout.dao;
-
-/**
- * Created by Tong on 06.15.
- */
-public class UserLogDao {
-}
---
+++ src/main/java/edu/fudan/hangout/service/LoginService.java
@@ -0,0 +1,10 @@
+package edu.fudan.hangout.service;
+
+import edu.fudan.hangout.bean.User;
+
+/**
+ * Created by Tong on 06.15.
+ */
+public interface LoginService {
+    int login(User user);
+}
