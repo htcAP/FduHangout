9d7d65d4acd38dbe980adb09c0ec01f64178f951
lifengshuang <b2010000327@gmail.com>
2015-06-16 18:55:36 +0800

    support JSON

--- src/main/java/edu/fdu/hangout/controller/LoginController.java
+++ src/main/java/edu/fdu/hangout/controller/LoginController.java
@@ -6,22 +6,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by lifengshuang on 6/16/15.
  */
 
 @Controller
+//@EnableWebMvc
 @RequestMapping("/login")
 public class LoginController {
 
     @RequestMapping(value = "{name}", method = RequestMethod.GET)
     public @ResponseBody
-    User login(@PathVariable String name) {
+    Object login(@PathVariable String name) {
         User user = new User();
         user.setPhone(name);
         user.setPassword("123321");
         return user;
     }
+
 
 }
