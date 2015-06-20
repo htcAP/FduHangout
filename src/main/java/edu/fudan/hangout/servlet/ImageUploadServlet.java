1a9784a403de478086bc64b7ba88060356d1a35d
lifengshuang <b2010000327@gmail.com>
2015-06-20 23:40:52 +0800

    test done

--- src/main/java/edu/fudan/hangout/servlet/ImageUploadServlet.java
+++ src/main/java/edu/fudan/hangout/servlet/ImageUploadServlet.java
@@ -13,7 +13,6 @@ import java.util.UUID;
  * Created by lifengshuang on 6/19/15.
  */
 
-@WebServlet(value = "/ffrfe/fwfefe")
 public class ImageUploadServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/java/edu/fudan/hangout/view/request/activity/GetActivityListView.java
+++ src/main/java/edu/fudan/hangout/view/request/activity/GetActivityListView.java
@@ -10,26 +10,18 @@ import javax.validation.constraints.NotNull;
 public class GetActivityListView {
 
     @NotNull(message = "100|缺少token字段")
-    private String token;
+    private Integer user_id;
 
-    @NotNull(message = "101|缺少activity_kind字段")
-    @Max(value = 2, message = "102|activity_kind = 0 or 1 or 2")
-    @Min(value = 0, message = "103|activity_kind = 0 or 1 or 2")
-    private Integer activity_kind;
+//    @NotNull(message = "101|缺少activity_kind字段")
+//    @Max(value = 2, message = "102|activity_kind = 0 or 1 or 2")
+//    @Min(value = 0, message = "103|activity_kind = 0 or 1 or 2")
+//    private Integer activity_kind;
 
-    public String getToken() {
-        return token;
+    public Integer getUser_id() {
+        return user_id;
     }
 
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Integer getActivity_kind() {
-        return activity_kind;
-    }
-
-    public void setActivity_kind(Integer activity_kind) {
-        this.activity_kind = activity_kind;
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
     }
 }
